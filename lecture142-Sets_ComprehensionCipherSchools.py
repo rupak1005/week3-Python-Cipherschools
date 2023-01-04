a6ee21099557f3b024dc5f5f4f3951b279036ede
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2023-01-05 01:27:36 +0530

    Add files via upload

---
+++ lecture142-Sets_ComprehensionCipherSchools.py
@@ -0,0 +1,9 @@
+# sets comprehension -----> only one video
+s = {k**2 for k in range(1,11)}
+print(s)
+
+names = ['Uttam', 'robo1', 'robo2']
+first = {name[0] for name in names}
+print(first)
+Footer
+© 2022 GitHub, Inc.
---
+++ lecture159-zip_function-CipherSchools.py
@@ -0,0 +1,10 @@
+# zip function
+user_id = ['user1', 'user2', 'user3']
+names = ['aayush', 'mohit', 'rohit']
+last_names = ['pathak', 'vashishtha','sharma']
+
+# ('user1','Uttam'),('user2','mohit')
+print(list(zip(user_id, names, last_names)))
+
+# example =[('a',1), ('b', 2)]
+# print(dict(example))
